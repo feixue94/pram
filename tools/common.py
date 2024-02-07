acadef39df928aa221e1ba15973bf3a8730201b2
feixue94 <feixue@pku.edu.cn>
2024-02-07 15:15:49 +0000

    add matchers

--- tools/common.py
+++ tools/common.py
@@ -50,3 +50,7 @@ def torch_set_gpu(gpus):
         print('Launching on CPU')
 
     return cuda
+
+
+def names_to_pair(name0, name1):
+    return '_'.join((name0.replace('/', '-'), name1.replace('/', '-')))
