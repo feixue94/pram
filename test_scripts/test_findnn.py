5ac68ddf1feaf51f6af386c0c402286b204164be
feixue94 <feixue@pku.edu.cn>
2024-12-09 13:18:33 +0000

    add nv

--- test_scripts/test_findnn.py
+++ test_scripts/test_findnn.py
@@ -32,12 +32,31 @@ if __name__ == '__main__':
     netvald = load_netvlad(
         weight_path='/scratches/flyer_2/fx221/Research/Code/third_weights/vgg16_netvlad_checkpoint/checkpoints/checkpoint.pth.tar')
     netvald = netvald.cuda().eval()
-    
+
     ref_image_root = '/scratches/flyer_3/fx221/dataset/Hospital/images/front'
     query_img_root = '/scratches/flyer_3/fx221/dataset/Hospital/query'
 
     ref_images = glob(ref_image_root + '/**/*.png', recursive=True)
+    print('Find {} reference images in {}'.format(len(ref_images), ref_image_root))
     query_images = glob(query_img_root + '/**/*.png', recursive=True)
+    print('Find {} query images in {}'.format(len(query_images), query_img_root))
 
     ref_images = sorted(ref_images)
-    query_feats = extract_netvald_feat(image_root=ref_image_root, image_names=ref_images, model=netvald)
+    ref_feats = extract_netvald_feat(image_root=ref_image_root, image_names=ref_images, model=netvald)  # [N, D]
+    query_feats = extract_netvald_feat(image_root=query_img_root, image_names=query_images, model=netvald)  # [M, D]
+    pairs = {}
+    dist = query_feats @ ref_feats.t()
+
+    topk = 20
+    values, ids = torch.topk(dist, dim=1, k=topk, largest=True)
+    for i in range(len(query_images)):
+        q_name = query_images[i]
+        candidates = []
+        for k in range(topk):
+            candidates.append(ref_images[ids[i][k]])
+        pairs[q_name] = candidates
+
+    with open('/scratches/flyer_3/fx221/dataset/Hospital/query/query-pairs-bag1-front-netvald-20.txt', 'w') as f:
+        for k in pairs.keys():
+            for c in pairs[k]:
+                f.write(k + ' ' + c + '\n')
